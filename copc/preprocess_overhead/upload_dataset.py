bdce56891301755349490b5cb58a38197018d653
Aitor Arjona <aitor.a98@gmail.com>
2022-11-28 17:08:44 +0100

    Create copc overhead script

--- copc/preprocess_overhead/upload_dataset.py
+++ copc/preprocess_overhead/upload_dataset.py
@@ -1,20 +1,20 @@
 import boto3
 import botocore.exceptions
-import requests
 import os
 import urllib3
 import concurrent.futures
 
 BUCKET = 'lithops-datasets'
 PREFIX = 'copc-preprocess-overhead/CA_YosemiteNP_2019'
-WORKERS = 4
+WORKERS = 16
 LINKS_FILENAME = 'dataset_links.txt'
 
 if __name__ == '__main__':
+    s3 = boto3.client('s3', region_name='us-east-1')
     def download_and_upload(link):
+        http = urllib3.PoolManager()
         filename = os.path.basename(link)
         key = os.path.join(PREFIX, filename)
-        s3 = boto3.client('s3')
 
         try:
             s3.head_object(Bucket=BUCKET, Key=key)
@@ -29,13 +29,12 @@ if __name__ == '__main__':
             print(f'Key {key} exists')
             return
 
-        head_res = requests.head(link)
+        head_res = http.request('HEAD', link)
         print(f'Uploading {filename} (size {head_res.headers["Content-Length"]})')
         response = http.request('GET', link, preload_content=False)
         s3.upload_fileobj(Bucket=BUCKET, Key=key, Fileobj=response)
         print(f'Put {key} OK')
 
-    http = urllib3.PoolManager()
 
     with open(LINKS_FILENAME, 'r') as file:
         links = file.readlines()
@@ -43,6 +42,6 @@ if __name__ == '__main__':
     futures = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as pool:
         for link in links:
-            f = pool.submit(download_and_upload, link)
+            f = pool.submit(download_and_upload, link.strip())
             futures.append(f)
         [f.result() for f in futures]
